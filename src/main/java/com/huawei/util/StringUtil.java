fa129992c6b66ee8042128a905ce82fd2f3bbddf
zhangziqiang1 <35445530+zhangziqiang1@users.noreply.github.com>
2022-08-10 12:48:23 +0800

    Add files via upload

---
+++ src/main/java/com/huawei/util/StringUtil.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ */
+
+package com.huawei.util;
+
+/**
+ * StringUtil
+ *
+ * @since 2022-08-10
+ */
+public class StringUtil {
+
+    public boolean isHuawei (){
+        return true;
+    }
+}
